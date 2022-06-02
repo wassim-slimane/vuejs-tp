988a4ffcce04fe73373054c099d1a53124e8d3c4
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-02 10:17:00 +0200

    exo 1 et 2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
+import {onMounted, ref} from "vue"
+const counter = ref(0);
+
+onMounted(() => {
+  setInterval(() => {
+    counter.value++
+  }, 1000);
+})
+
+
 </script>
 
 <template>
-  
+
+<h1>Ma page</h1>
+
+<p>counter : {{ counter }}</p>
+<input type="number" v-model="counter">
+<button @click="counter=0">Reset counter</button>
 </template>
 
 <style>
