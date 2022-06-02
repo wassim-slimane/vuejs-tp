66fbb7aa69696748d910349647e466c2dafa0abc
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-02 11:08:27 +0200

    exo4

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script setup>
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import * as jose from 'jose';
 
 const counter = ref(0);
 const token = ref(null);
+const tokenStocked = ref(null);
 
 onMounted(() => {
+  if(localStorage.jwt) {
+    tokenStocked.value = localStorage.jwt;
+  }
   setInterval(() => {
     counter.value++
   }, 1000);
 })
 
 function submit() {
-  const claims = jose.decodeJwt(token.value);
-  console.log(claims);
+  try {
+    const claims = jose.decodeJwt(token.value);
+    console.log(claims);
+    localStorage.jwt = token.value;
+    tokenStocked.value = token.value;
+  } catch(exc) {
+    console.log("Exception : " + exc.message);
+  }
 }
 
 </script>
@@ -28,6 +38,8 @@ function submit() {
 <form action="#">
   <input type="text" name="jwt" placeholder="Rentrez le JWT" v-model="token" />
   <button @click.prevent="submit">Valider</button>
+  <p v-if="tokenStocked">JWT stocké : {{ tokenStocked }}</p>
+  <p v-else>Pas de JWT stocké</p>
 </form>
 </template>
 
