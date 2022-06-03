18dc191c600b96006e038bf53770c7014ce3b01f
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-03 12:16:12 +0200

    creation du formulaire d'inscription

--- src/components/FormInscriptionComponent.vue
+++ src/components/FormInscriptionComponent.vue
@@ -1,9 +1,27 @@
 <script setup>
+import { ref } from "@vue/reactivity";
+import { fakeSignin } from "../services/userStore";
+
+const name = ref(null);
+const email = ref(null);
+
 </script>
 
 <template>
-
-<button class="btn btn-primary">Valider</button>
+<h2>Inscription</h2>
+<div>
+    <form @submit.prevent="fakeSignin">
+        <div class="form-groupe">
+            <label for="name">Name : </label>
+            <input class="form-control" type="text" v-model="name">
+        </div>
+        <div class="form-groupe">
+            <label for="name">email : </label>
+            <input class="form-control" type="text" v-model="email">
+        </div>
+        <button type="submit" class="btn btn-primary">Valider</button>
+    </form>
+</div>
     
 </template>
 
